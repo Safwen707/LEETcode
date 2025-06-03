53a396280a02ba99c2a50563280d5178fd6d2f68
Safwen707 <152983113+Safwen707@users.noreply.github.com>
2025-06-03 22:08:03 +0100

    Time: 0 ms (100.00%) | Memory: 10.5 MB (87.67%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,33 @@
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+void backtrack(vector<int>& permutation, vector<int>& nums, vector<vector<int>>& res, vector<bool>& used) {
+            int len = nums.size();
+            if (permutation.size() == len) {
+                res.push_back(permutation);
+                return;
+            }
+
+            rep(i, 0, len) {
+                if (!used[i]) {
+                    permutation.push_back(nums[i]);
+                    used[i] = true;
+                    backtrack(permutation, nums, res, used);
+                    permutation.pop_back();
+                    used[i] = false;
+                }
+            }
+        };
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> permutation;
+        int len = nums.size();
+        vector<bool> used(len, false);
+        
+        
+
+        backtrack(permutation, nums, res, used);
+        return res;
+    }
+};
