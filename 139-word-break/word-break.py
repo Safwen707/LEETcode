23315c44a4cc9cd29a1b1152655d9bd6f5a3f5ed
Safwen707 <152983113+Safwen707@users.noreply.github.com>
2025-10-12 00:05:50 +0200

    Time: 7 ms (24.04%) | Memory: 18 MB (45.03%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,16 +1,16 @@
-from typing import List
-
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        wordSet = set(wordDict)
-        n = len(s)
-        dp = [False] * (n + 1)
-        dp[0] = True  # chaîne vide = segmentable
-
-        for i in range(1, n + 1):
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool: 
+        n= len(s)
+        dp=[False]*(n+1)
+        dp[0]=True
+        for i in range(1,n+1):
+            
             for j in range(i):
-                if dp[j] and s[j:i] in wordSet:
-                    dp[i] = True
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i]=True
                     break
 
+
+
         return dp[n]
+        
